2f8d8af1ab91517c007fc995754a2b8d371a3020
JohnWalrus <juan.aguero314@gmail.com>
2020-11-03 18:28:41 -0300

    topbar complete - online section progress

--- src/components/Contactos/index.js
+++ src/components/Contactos/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import FriendsButton from '../FriendsButton';
 import styles from './style.module.css';
-import SettingButton from '../SettingButton';
 import Avatar from '../Avatar';
 
 function Contactos({people=[]}) {
@@ -10,28 +9,38 @@ function Contactos({people=[]}) {
     <div className={styles.root}> 
       
       <div className={styles.topbar}>
-          <div className={styles.friends}>  
-            <span className="material-icons">
-              emoji_people
-            </span>
-            <p>Amigos</p>
+          <div className={styles.topone}>
+            <div className={styles.friends}>  
+              <span className="material-icons">
+                emoji_people
+              </span>
+              <p>Amigos</p>
+            </div>
+            <div className={styles.spacebar}/>
+            <FriendsButton>Conectado</FriendsButton>
+            <FriendsButton>Todos</FriendsButton>
+            <FriendsButton>Pendiente</FriendsButton>
+            <FriendsButton>Bloqueado</FriendsButton>
+            <div className={styles.addfriend}>Añadir amigo</div>
           </div>
-          <div className={styles.spacebar}/>
-          <FriendsButton>Amigos</FriendsButton>
-          <FriendsButton>Todos</FriendsButton>
-          <FriendsButton>Pendiente</FriendsButton>
-          <FriendsButton>Bloqueado</FriendsButton>
-          <FriendsButton>Añadir Amigo</FriendsButton>
-          <SettingButton icon="chat"/>
-          <div className={styles.spacebar}/>
-          <SettingButton icon="inbox"/>
-          <SettingButton icon="help"/>
-          
+
+          <div className={styles.toptwo}>
+            <span className="material-icons">
+            chat
+            </span>
+            <div className={styles.spacebar}/>
+            <span className="material-icons">
+            inbox
+            </span>
+            <span className="material-icons">
+            help
+            </span>
+          </div>
         </div>
 
-        <div className={styles.botbox}>
-
+      <div className={styles.botbox}>
           <div className={styles.onlinelist}>
+            <div className={styles.conectado}>CONECTADO -- 5</div>
             {people.filter(person=>person.estado).map((person, i) => <div className={styles.contact} key={i}>
               <div className={styles.content}>
                 <div className={styles.conperson}>
@@ -42,6 +51,8 @@ function Contactos({people=[]}) {
                   <span className="material-icons">
                     chat_bubble
                   </span>
+                </div>
+                <div className={styles.conicon}>
                   <span className="material-icons">
                     more_vert
                   </span>
