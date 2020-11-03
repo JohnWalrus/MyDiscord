9bf65c7cdb4fc6cb7f53b820f3fffe0f63eb5496
Jules Morelli <jmorelli@manas.tech>
2020-11-03 18:43:42 -0300

    Move servers const to its own file

--- src/App.js
+++ src/App.js
@@ -1,62 +1,13 @@
 import React from 'react';
 import USER_STATUS from './constants/user_status';
+import SERVERS from './constants/servers';
 import './App.css';
 import Sidebar from "./components/Sidebar"
 import ChatBar from "./components/ChatBar"
 import Contactos from "./components/Contactos"
 
 function App() {
-  const servers = [
-    {
-      id:1,
-      image: `${process.env.PUBLIC_URL}/Brachypelma-hamorii.jpg`,
-    },
-    {
-      id:2,
-      image: `${process.env.PUBLIC_URL}/cornfed.png`,
-    },
-    {
-      id:3,
-      image: `${process.env.PUBLIC_URL}/juorgullosa.jpg`,
-    },
-    {
-      id:4,
-      image:`${process.env.PUBLIC_URL}/kid-bastion.jpg`,
-    },
-    {
-      id:5,
-      image:`${process.env.PUBLIC_URL}/life.jpg`,
-    },
-    {
-      id:6,
-      image:`${process.env.PUBLIC_URL}/madre.jpg`,
-    },
-    {
-      id:7,
-      image:`${process.env.PUBLIC_URL}/mamimi.jpg`,
-    },
-    {
-      id:8,
-      image:`${process.env.PUBLIC_URL}/maxresdefault.jpg`,
-    },
-    {
-      id:9,
-      image:`${process.env.PUBLIC_URL}/Nacho_Enero_2020_color.jpg`,
-    },
-    {
-      id:10,
-      image:`${process.env.PUBLIC_URL}/navidad 2.png`,
-    },
-    {
-      id:11,
-      image:`${process.env.PUBLIC_URL}/Poecilotheria metallica.png`,
-    },
-    {
-      id:12,
-      image:`${process.env.PUBLIC_URL}/punpun.jpg`,
-    },
-
-  ];
+  
   const people = [
     {
       image: `${process.env.PUBLIC_URL}/Brachypelma-hamorii.jpg`,
@@ -116,7 +67,7 @@ function App() {
 
   return (
     <>
-      <Sidebar servers={servers}/>
+      <Sidebar servers={SERVERS}/>
       <ChatBar people={people}/>
       <Contactos people={people}/>
      
